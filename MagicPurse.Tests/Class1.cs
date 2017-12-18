3ccdacc30cc255f43a7b0cbf1bf5a3b97f8d32ed
Matteo Cagno <matteo.cagno@gmail.com>
2017-12-18 22:57:28 +0100

    Add project files.

---
+++ MagicPurse.Tests/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MagicPurse.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        [TestCase(6, 4)]
+        [TestCase(20, 77)]
+        [TestCase(24, 141)]
+        [TestCase(48, 2377)]
+        public void FirstTest(long number, long expectedResult)
+        {
+            MagicPurse purse =new MagicPurse();
+            long result = purse.MakeEvenChange(number);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ MagicPurse/MagicPurse.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MagicPurse
+{
+    public class MagicPurse
+    {
+        private long[] coins =
+        {
+            1, //Halfpenny 0.5d
+            2, //Penny 1d
+            6, //Threepence 3d
+            12, //Sixpence 6d
+            24, //Shilling 1/-
+            48, //Two shillings 
+            60  //Two shillings and sixpence
+            
+        };
+
+        private List<long[]> _combinations = new List<long[]>();
+
+        public long MakeEvenChange(long number)
+        {
+            long[] combination = new long[coins.Length];
+
+            GetCombi(number, coins.Length - 1, combination);
+            long sum = 0;
+            foreach (var combi in _combinations)
+            {
+                sum += PermutationWithRepetitions(combi);
+            }
+
+            return sum;
+        }
+
+        private void GetCombi(long number, int i, long[] combination)
+        {
+
+            long division = number / coins[i];
+
+            if (i == 0)
+            {
+                combination[i] = division;
+                if (combination.Sum() % 2 == 0)
+                {
+                    _combinations.Add((long[]) combination.Clone());
+                }
+                return;
+            }
+
+            for (int k = 0; k <= division; k++)
+            {
+                combination[i] = k;
+                long remainder = number - coins[i] * k;
+                if (remainder > 0)
+                {
+                    GetCombi(remainder, i - 1, combination);
+                }
+
+            }
+        }
+
+        private long Factorial(long x)
+        {
+            if (x <= 1)
+            {
+                return 1;
+            }
+            return x * Factorial(x - 1);
+        }
+
+        private long PermutationWithRepetitions(long[] combinations)
+        {
+            long denominator = 1;
+            long numerator = Factorial(combinations.Sum());
+            for (int i = 0; i < combinations.Length; i++)
+            {
+                denominator *= Factorial(combinations[i]);
+            }
+            return numerator / denominator;
+        }
+    }
+}
